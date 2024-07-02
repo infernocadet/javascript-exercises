ed33b7adc4ddbf4be77a81ea2b1c7c7701e502aa
Marc Caramoan <caramoanmarc@gmail.com>
2024-07-02 11:27:34 +1000

    done w/ exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (myArray, ...args) {
+  return myArray.filter((x) => !args.includes(x));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (numOne, numTwo) {
+  if (typeof numOne !== "number" || typeof numTwo !== "number") {
+    return "ERROR";
+  }
 
+  if (numOne < 0 || numTwo < 0) {
+    return "ERROR";
+  }
+
+  let first;
+  let end;
+  let total = 0;
+  if (numOne >= numTwo) {
+    first = numOne;
+    end = numTwo;
+  } else {
+    first = numTwo;
+    end = numOne;
+  }
+
+  for (first; first > end - 1; first--) {
+    total += first;
+  }
+  return total;
 };
 
 // Do not edit below this line
